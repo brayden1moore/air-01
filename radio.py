1918b451e7f2cc040b396c1f860ac37ece91e1a3
Brayden Moore <braydenmoore2001@gmail.com>
2025-04-04 21:47:27 -0700

    backlight off

--- radio.py
+++ radio.py
@@ -120,7 +120,7 @@ disp = st7789.ST7789(
     dc=9,            # BCM pin used for data/command
     backlight=None,  # We'll control the backlight ourselves
     # backlight=13,  # 13 for Pirate-Audio; 18 for back BG slot, 19 for front BG slot.
-    spi_speed_hz=SPI_SPEED_MHZ * 1000 * 1000
+    spi_speed_hz=SPI_SPEED_MHZ * 1000 * 2000
 )
 
 disp.begin()
