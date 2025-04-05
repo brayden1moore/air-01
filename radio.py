ad65b5693975b9b162d3618f21498e2dea307f32
Brayden Moore <braydenmoore2001@gmail.com>
2025-04-04 23:16:31 -0700

    add controls

--- radio.py
+++ radio.py
@@ -296,10 +296,13 @@ def display_info(name, play_status):
         draw.text((19, 195), name, font=font, fill=(255, 255, 255))
         draw.text((19, 205), "No description.", font=font, fill=(255, 255, 0))
     
+    prev_stream = stream_list[stream_list.index(name)-1]
+    next_stream = stream_list[stream_list.index(name)+1]
+
     draw.text((32, 10), '[play/pause]', font=font, fill=(100, 100, 100))
     draw.text((160, 10), '[random]', font=font, fill=(100, 100, 100))
-    draw.text((34, 224), f'[{stream_list[stream_list.index(name)-1]}]', font=font, fill=(100, 100, 100))
-    draw.text((162, 224), f'[{stream_list[stream_list.index(name)+1]}]', font=font, fill=(100, 100, 100))
+    draw.text((34, 224), f'[{prev_stream}]', font=font, fill=(100, 100, 100))
+    draw.text((162-len(next_stream)*12, 224), f'[{next_stream}]', font=font, fill=(100, 100, 100))
     safe_display(image)
 
 
