cf2470d954311a7a04be10ccf98119624b50647c
Brayden Moore <braydenmoore2001@gmail.com>
2025-04-15 13:30:42 -0700

    add locations

--- radio.py
+++ radio.py
@@ -49,85 +49,106 @@ streams = {
         'name': 'NTS 1',
         'stream': 'https://stream-relay-geo.ntslive.net/stream?client=NTSWebApp&device=800913353.1735584982',
         'info': 'https://www.nts.live/api/v2/live',
-        'logo': 'nts1.png'
+        'logo': 'nts1.png',
+        'location': 'London'
     },
     'NTS 2': {
         'name': 'NTS 2',
         'stream': 'https://stream-relay-geo.ntslive.net/stream2?client=NTSWebApp&device=800913353.1735584982',
         'info': 'https://www.nts.live/api/v2/live',
-        'logo': 'nts2.png'
+        'logo': 'nts2.png',
+        'location': 'London'
     },
     'Dublab': {
         'name': 'Dublab',
         'stream': 'https://dublab.out.airtime.pro/dublab_a',
         'info': 'https://www.dublab.com/.netlify/functions/schedule?tz=America%2FLos_Angeles',
-        'logo': 'dublab.jpeg'
+        'logo': 'dublab.jpeg',
+        'location': 'Los Angeles'
     },
     'WNYU': {
         'name': 'WNYU',
         'stream': 'http://cinema.acs.its.nyu.edu:8000/wnyu128.mp3',
         'info': 'https://wnyu.org/v1/schedule/current_and_next',
-        'logo': 'wnyu.jpeg'
+        'logo': 'wnyu.jpeg',
+        'location': 'New York'
     },
     'Voices': {
         'name': 'Voices',
         'stream': 'https://voicesradio.out.airtime.pro/voicesradio_a',
         'info': 'https://voicesradio.airtime.pro/api/live-info-v2?timezone=America/Los_Angeles',
-        'logo': 'voices.jpeg'
+        'logo': 'voices.jpeg',
+        'location': 'London'
     },
     'Bloop Radio': {
         'name': 'Bloop Radio',
         'stream': 'https://radio.canstream.co.uk:8058/live.mp3',
         'info': 'https://blooplondon.com/wp-admin/admin-ajax.php?action=radio_station_current_show',
-        'logo': 'bloop.png'
+        'logo': 'bloop.png',
+        'location': 'London'
     },
     'Radio Quantica': {
         'name': 'Radio Quantica',
         'stream': 'https://stream.radioquantica.com:8443/stream',
         'info': 'https://api.radioquantica.com/api/live-info',
-        'logo': 'quantica.jpeg'
+        'logo': 'quantica.jpeg',
+        'location': 'Lisbon'
     },
     'HydeFM': {
         'name': 'HydeFM',
         'stream': 'https://media.evenings.co/s/DReMy100B',
         'info': 'https://api.evenings.co/v1/streams/hydefm/public',
-        'logo': 'hydefm.png'
+        'logo': 'hydefm.png',
+        'location': 'San Francisco'
     },
     'Do!!You!!!': {
         'name': 'Do!!You!!!',
         'stream': 'https://doyouworld.out.airtime.pro/doyouworld_a',
         'info': 'https://doyouworld.airtime.pro/api/live-info-v2',
-        'logo': 'doyou.png'
+        'logo': 'doyou.png',
+        'location': 'Los Angeles'
     },
     'SutroFM': {
         'name': 'SutroFM',
         'stream': 'https://media.evenings.co/s/7Lo66BLQe',
         'info': 'https://api.evenings.co/v1/streams/sutrofm/public',
-        'logo': 'sutrofm.jpeg'
+        'logo': 'sutrofm.jpeg',
+        'location': 'San Francisco'
     },
     'KQED': {
         'name': 'KQED',
         'stream': 'https://streams.kqed.org/kqedradio?onsite=true',
         'info': 'https://media-api.kqed.org/radio-schedules/',
-        'logo': 'kqed.png'
+        'logo': 'kqed.png',
+        'location': 'San Francisco'
     },
     'Lower Grand Radio': {
         'name': 'Lower Grand Radio',
         'stream': 'https://lowergrandradio.out.airtime.pro:8000/lowergrandradio_a',
         'info': 'https://lowergrandradio.airtime.pro/api/live-info-v2',
-        'logo': 'lgr.png'
+        'logo': 'lgr.png',
+        'location': 'Oakland'
+    },
+    'We Are Various': {
+        'name': 'We Are Various',
+        'stream': 'https://azuracast.wearevarious.com/listen/we_are_various/live.mp3',
+        'info': 'https://azuracast.wearevarious.com/api/nowplaying/we_are_various',
+        'logo': 'various.jpeg',
+        'location': 'Antwerp'
     },
     'Kiosk Radio': {
         'name': 'Kiosk Radio',
         'stream': 'https://kioskradiobxl.out.airtime.pro/kioskradiobxl_b',
         'info': 'https://kioskradiobxl.airtime.pro/api/live-info-v2',
-        'logo': 'kiosk.webp'
+        'logo': 'kiosk.webp',
+        'location': 'Brussels'
     },
     'Internet Public Radio': {
         'name': 'Internet Public Radio',
         'stream': 'https://c11.radioboss.fm:18270/stream?_ic2=1744737620752',
         'info': 'https://c11.radioboss.fm/w/nowplayinginfo?u=270&1744737649518',
-        'logo': 'internet.png'
+        'logo': 'internet.png',
+        'location': 'Guadalajara'
     }
         #'BlueMoonRadio': {
     #    'name': 'BlueMoonRadio',
@@ -283,6 +304,7 @@ def display_info(name):
 
     show_names = []
     descriptions = []
+    locations = []
 
     # evening
     if name in ['HydeFM','SutroFM']:
@@ -291,6 +313,7 @@ def display_info(name):
         show_title = info.get('name', name)
         num_listeners = info['listeners']
         listeners = f"{num_listeners} listener{s(num_listeners)}."
+        locations.append(stream_info['location'])
 
         if status == False:
             show_names.append(name)
@@ -303,8 +326,8 @@ def display_info(name):
         info = requests.get(stream_info['info']).json()
         result_idx = 0 if name == 'NTS 1' else 1
         show_info = info['results'][result_idx]['now']
-
         description = show_info['embeds']['details']['description']
+        location = show_info['embeds']['details']['location_long']
         if not description:
             genres = []
             for g in show_info['embeds']['details']['genres']:
@@ -312,6 +335,11 @@ def display_info(name):
                 descriptions.append(', '.join(genres))
         else:
             descriptions.append(description)
+        if not location:
+            locations.append(stream_info['location'])
+        else: 
+            locations.append(location)
+
         show_names.append(show_info['broadcast_title'])
 
     elif name == 'KQED':
@@ -320,20 +348,22 @@ def display_info(name):
         info_url = stream_info['info'] + today
         info = requests.get(info_url).json()
         programs = info['data']['attributes']['schedule']
-        show_name = 'KQED'
-        description = 'No description.'
+        show_name = name
+        description = 'No description.'        
         for program in programs:
             if int(program['startTime']) < epoch_time:
                 show_name = program['programTitle']
                 description = program['programDescription']
+        
         show_names.append(show_name)
+        locations.append(stream_info['location'])
         descriptions.append(description) 
 
     elif name == 'Dublab':
         now = datetime.now(timezone.utc)
         info_url = stream_info['info']
         info = requests.get(info_url).json()
-        show_name = 'Dublab'
+        show_name = name
         description = 'No description.'
         for program in info:
             if datetime.fromisoformat(program['startTime']) < now:
@@ -341,7 +371,8 @@ def display_info(name):
                 description = program['eventTitleMeta']['eventName']
                 logo_url = program['attachments']
         show_names.append(show_name)
-        descriptions.append(description) 
+        descriptions.append(description)
+        locations.append(stream_info['location']) 
     
     elif name == 'WNYU':
         info_url = stream_info['info']
@@ -353,13 +384,15 @@ def display_info(name):
         description = ', '.join([i.title() for i in info['episode']['genre_list']])
         show_names.append(show_name)
         descriptions.append(description) 
+        locations.append(stream_info['location']) 
     
     elif name == 'Radio Quantica':
         info_url = stream_info['info']
         info = requests.get(info_url).json()
-        description = info['currentShow'][0]['name']
-        descriptions.append(description)
-        show_names.append('Radio Quantica')
+        show_name = info['currentShow'][0]['name']
+        show_names.append(show_name)
+        locations.append(stream_info['location']) 
+        descriptions.append('No description.')
 
     # airtime
     elif name in ['Do!!You!!!','Voices','Lower Grand Radio','Kiosk Radio']:
@@ -372,9 +405,9 @@ def display_info(name):
             show_name = name
             description = 'Is offline.'
         
-        print(description)
         show_names.append(show_name)
         descriptions.append(description) 
+        locations.append(stream_info['location']) 
 
     elif name == 'Bloop Radio':
         url = stream_info['info']
@@ -388,14 +421,23 @@ def display_info(name):
             'User-Agent': 'Mozilla/5.0'
         }
         response = requests.post(url, data=payload, headers=headers).json()
-        descriptions.append(response['current-show']['showName'])
-        show_names.append(name)
+        show_names.append(response['current-show']['showName'])
+        locations.append(stream_info['location']) 
+        descriptions.append('No description.')
     
     elif name == 'Internet Public Radio':
         url = stream_info['info']
         info = requests.get(url).json()
-        show_names.append(name)
-        descriptions.append(info['nowplaying'])
+        show_names.append(info['nowplaying'])
+        locations.append(stream_info['location']) 
+        descriptions.append('No description.')
+
+    elif name == 'We Are Various':
+        url = stream_info['info']
+        info = requests.get(url).json()
+        show_names.append(info['now_playing']['title'])
+        locations.append(stream_info['location']) 
+        descriptions.append('No description.')
 
     font = ImageFont.load_default()
     image = current_image.copy()
@@ -405,12 +447,15 @@ def display_info(name):
     image.paste(background, (24, 195))
 
     try:
-        show_names = [i.replace('&amp;','&').replace('\u2019', "'").replace('\u2013', "-").replace('&#039;',"'").replace('\u201c','"').replace('\u201d','"') for i in show_names]
-        descriptions = [i.replace('&amp;','&').replace('\u2019', "'").replace('\u2013', "-").replace('&#039;',"'").replace('\u201c','"').replace('\u201d','"') for i in descriptions]
-        draw.text((24, 195), show_names[0][:40], font=font, fill=(255, 255, 255))
-        draw.text((24, 205), descriptions[0][:40], font=font, fill=(255, 255, 0))
+        show_names = [i.replace('&amp;','&').replace('\u2019', "'").replace('\u2013', "-").replace('&#039;',"'").replace('\u201c','"').replace('\u201d','"').replace('\n',' ') for i in show_names]
+        descriptions = [i.replace('&amp;','&').replace('\u2019', "'").replace('\u2013', "-").replace('&#039;',"'").replace('\u201c','"').replace('\u201d','"').replace('\n',' ') for i in descriptions]
+        
+        title = f'{name} ({locations[0]})'
+        
+        draw.text((24, 195), title, font=font, fill=(255, 255, 255))
+        draw.text((24, 205), show_names[0][:40], font=font, fill=(255, 255, 0))
     except:
-        draw.text((24, 195), name, font=font, fill=(255, 255, 255))
+        draw.text((24, 195), title, font=font, fill=(255, 255, 255))
         draw.text((24, 205), "No description.", font=font, fill=(255, 255, 0))
 
     safe_display(image)
