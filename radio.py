10a97d86d8601a3ca75c8d21e3d4fb7d16b4b2ed
Brayden Moore <braydenmoore2001@gmail.com>
2025-07-02 10:08:52 -0700

    encoder code

--- radio.py
+++ radio.py
@@ -325,16 +325,16 @@ def show_volume_overlay(volume):
         img = original_image_before_volume.copy()
         draw = ImageDraw.Draw(img)
         
-        bar_width = 10
-        bar_height = 100
-        bar_x = 15 #(SCREEN_WIDTH - bar_width) // 2
-        bar_y = 20 #SCREEN_HEIGHT - 30
+        bar_width = 320
+        bar_height = 5
+        bar_x = 0 #(SCREEN_WIDTH - bar_width) // 2
+        bar_y = 0 #SCREEN_HEIGHT - 30
         
         draw.rectangle([bar_x-2, bar_y-2, bar_x+bar_width+2, bar_y+bar_height+2], fill=BORDER_COLOR)
         draw.rectangle([bar_x, bar_y, bar_x+bar_width, bar_y+bar_height], fill=BACKGROUND_COLOR)
         
-        volume_height = int((volume / 100) * bar_height)
-        draw.rectangle([bar_x, bar_height+bar_y, bar_x+bar_width, bar_y+(bar_height-volume_height)], fill=TEXT_COLOR)
+        volume_width = int((volume / 100) * bar_width)
+        draw.rectangle([bar_x, bar_y, bar_x+volume_width, bar_y+bar_height], fill=TEXT_COLOR)
         
         safe_display(img)
 
