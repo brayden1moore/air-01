8f155275a109765e58c38a9a7dcd31fecdb60c1f
Brayden Moore <braydenmoore2001@gmail.com>
2025-08-14 15:19:56 -0700

    add wifi

--- wifi.py
+++ wifi.py
@@ -23,5 +23,21 @@ print('thx')
 result = subprocess.run(['nmcli', 'dev','wifi' ,'connect' ,ssid ,'password' ,password],
                stdout=subprocess.PIPE,
                text=True, check=True)
-resultoutput = result.stdout.strip()
-print('ok',resultoutput)
+ 
+import socket
+
+def internet(host="8.8.8.8", port=53, timeout=3):
+    """
+    Host: 8.8.8.8 (google-public-dns-a.google.com)
+    OpenPort: 53/tcp
+    Service: domain (DNS/TCP)
+    """
+    try:
+        socket.setdefaulttimeout(timeout)
+        socket.socket(socket.AF_INET, socket.SOCK_STREAM).connect((host, port))
+        return True
+    except socket.error as ex:
+        print(ex)
+        return False
+
+internet()
